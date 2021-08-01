b5d5e22f55ebf13e2452adf3951dd02ac97911db
Sherikshaxe <34997904+Sherikshaxe@users.noreply.github.com>
2021-08-01 19:49:25 +0500

    Completed task!

---
+++ alif.local/adl.php
@@ -0,0 +1,13 @@
+<?php
+
+function echonl (string $s) : void {
+
+	echo $s . PHP_EOL;
+
+}
+
+function error(string $s) : void
+{
+	echonl($s);
+	exit(1);
+}
---
+++ alif.local/index.php
@@ -0,0 +1,55 @@
+<?php
+
+require 'adl.php';
+
+	
+	$dates = file_get_contents("secret.txt");
+	$sliceddates = explode(" ", $dates);
+	$all  = array();
+	$plus = array();
+	$minus = array();
+	$zarb = array();
+	$taqsim = array();
+	$negatives= array();
+	$positives= array();
+	$fl = array();
+		for ($i=0; $i<count($sliceddates); $i+=2) {
+		     array_push($all,$sliceddates[$i]+$sliceddates[$i+1]);
+		     array_push($fl, $sliceddates[$i]."+".$sliceddates[$i+1]."=");
+		     array_push($all,$sliceddates[$i]-$sliceddates[$i+1]);
+		     array_push($fl, $sliceddates[$i]."-".$sliceddates[$i+1]."=");
+		     array_push($all,$sliceddates[$i]*$sliceddates[$i+1]);
+		     array_push($fl, $sliceddates[$i]."*".$sliceddates[$i+1]."=");
+		     array_push($all,$sliceddates[$i]/$sliceddates[$i+1]);
+		     array_push($fl, $sliceddates[$i].":".$sliceddates[$i+1]."=");
+
+		}
+
+		for ($j=0; $j<count($all); $j++) {
+			 	if ($all[$j]>0) {
+			 		
+			 	array_push($positives,$fl[$j].$all[$j]."
+");			
+			    }else{
+			    array_push($negatives, $fl[$j].$all[$j]."
+");
+			    }
+			
+			
+		}
+		echo "POSiTIVE ANSWERS HERE:
+";
+			for ($o=0; $o<count($positives); $o++) { 
+				echo $positives[$o];
+			}
+		echo "NEGATIVE ANSWERS HERE:
+";
+			for ($o=0; $o<count($negatives); $o++) { 
+				echo $negatives[$o];
+			}
+		echo "LOOK AT FILE SECRET.txt";
+
+
+		file_put_contents("positives.txt", $positives);	
+		file_put_contents("negatives.txt", $negatives);
+	
